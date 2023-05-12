35f5fd50839c4d5045d1e0d20cc9c127b9469949
Nesb01t <nesb01t@hotmail.com>
2023-05-13 01:46:38 +0800

    Update feature1

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
-var age = 1;
-console.log(age);
+const Koa = require("koa");
+const app = new Koa();
+
+const router = require("./router/index.js");
+const errorCollector = require("./middleware/errorCollector.js");
+
+const port = 3000;
+
+app.use(async (ctx, next) => {
+  await next();
+  if (parseInt(ctx.status === 404)) {
+    ctx.response.redirect("/404");
+  }
+});
+app.use(errorCollector);
+app.use(router.routes(), router.allowedMethods());
+app.listen(port, () => {
+  console.log("Server is running at 3000");
+});
